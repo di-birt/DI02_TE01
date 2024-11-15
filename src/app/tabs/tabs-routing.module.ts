67a75f315f1d8c7a92bf70b21f4395921f46242e
di-birt <mardanza@birt.eus>
2024-11-15 11:35:53 +0100

    first commit

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -9,16 +9,16 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () => import('./tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () => import('./tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
+        path: 'ayuda',
+        loadChildren: () => import('./ayuda/ayuda.module').then(m => m.ayudaPageModule)
+      },      
       {
         path: '',
         redirectTo: '/tabs/tab1',
@@ -31,6 +31,7 @@ const routes: Routes = [
     redirectTo: '/tabs/tab1',
     pathMatch: 'full'
   }
+
 ];
 
 @NgModule({
